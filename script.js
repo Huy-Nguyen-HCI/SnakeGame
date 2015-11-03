ffb7f625c9598284a18e8f5912cc0f5fe2a88d70
Thang Le <amphale1996@gmail.com>
2015-11-02 23:25:43 -0500

    Keyboard input

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 $(document).ready(function() {
 	var c = $('#c')[0];
 	var ctx = c.getContext("2d");
-	
-	ctx.fillRect(0, 0, 50, 50);
+	ctx.fillStyle = 'blue';
+
+	function Rectangle(x, y, width, height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+
+		this.draw = function() {
+			ctx.fillStyle = 'blue';
+			ctx.fillRect(this.x, this.y, this.width, this.height);
+		}
+
+		this.clear = function() {
+			ctx.fillStyle = 'white';
+			ctx.fillRect(this.x, this.y, this.width, this.height);
+		}
+	} 
+
+	var snakePart = new Rectangle(0, 0, 50, 50);
+	snakePart.draw();
+
+	var k = new Kibo();
+
+	k.down(['down', 's'], function() {
+		snakePart.clear();
+		snakePart.y += 10;
+		snakePart.draw();
+	});
 });
