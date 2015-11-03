ef2bfa7b5fff4cdb0ca3b5c0bf196ab8e52b0013
HuyLafa <anhhuy0706@gmail.com>
2015-11-02 22:57:52 -0500

    Create a class Rectangle for snake part

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
-	var c = $('#c')[0];
+	var c = $("#c")[0];
 	var ctx = c.getContext("2d");
+	ctx.fillStyle = "blue";
+
+	function Rectangle(x, y, width, height) {
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
 	
-	ctx.fillRect(0, 0, 50, 50);
+	this.draw = function() {
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+}
+} 
+
+	var snakePart = new Rectangle(0, 0, 50, 50);
+	snakePart.draw();
 });
+
+
